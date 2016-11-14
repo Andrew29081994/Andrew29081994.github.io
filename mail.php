86053361f39ce6e37de74e2529d2c887345027d1
Andrew Gazhala <141.52nr@gmail.com>
2016-11-14 13:07:40 +0200

    Commit26

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
 <?php
 
-$recepient = "youmail@ya.ru";
+$recepient = "andreygazhala@gmail.com";
 $sitename = "Название сайта";
 
-$name = trim($_GET["name"]);
-$phone = trim($_GET["phone"]);
-$text = trim($_GET["text"]);
+$name = trim($_POST["name"]);
+$email = trim($_POST["tel"]);
+$text = trim($_POST["text"]);
+$message = "Имя: $name \nEmail: $email \nТекст: $text";
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
